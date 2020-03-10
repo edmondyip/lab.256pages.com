0153d00ea2578162a2b6a72db3fbd3de8ce81ade
Edmond <edmond.yip@glassesgallery.com>
2020-03-10 16:00:21 +0800

    update

--- pages/about.vue
+++ pages/about.vue
@@ -10,14 +10,14 @@
     h2 // About This Website
     p This page built by Threejs, Nuxtjs.
     p Server using Nodejs with Docker.
-    h2 // Update History
-    p v1.6 Limit height for some effect
-    p v1.5 Add Wave
-    p v1.4 Update UI
-    p v1.3 Add Hammer
-    p v1.2 Add Tree
-    p v1.1 Add Video
-    p v1.0 Public website
+    //- h2 // Update History
+    //- p v1.6 Limit height for some effect
+    //- p v1.5 Add Wave
+    //- p v1.4 Update UI
+    //- p v1.3 Add Hammer
+    //- p v1.2 Add Tree
+    //- p v1.1 Add Video
+    //- p v1.0 Public website
     h2 Material from
     p
       a(href="https://hdrihaven.com/" rel="noopener noreferrer" target="_blank") HDRI Haven
@@ -25,7 +25,7 @@
 
 <style lang="stylus" scoped>
   section
-    position fixed
+    position absolute
     pointer-events auto
     width 50%
     // height 100vh
@@ -35,6 +35,7 @@
     padding 20px
     color #ffffff
     z-index 9999
+    bottom 35px
     h1
       font-size 2.5rem
     h2
